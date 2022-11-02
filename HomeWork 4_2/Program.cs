e52b566cd9ce49c1cd7ec04a3a35ff9e9c2115ce
Владимир Александрович Кирдяшкин <kva@vast.su>
2022-11-02 09:07:07 +0300

    Изм

--- HomeWork 4_2/Program.cs
+++ HomeWork 4_2/Program.cs
@@ -10,47 +10,46 @@ namespace HomeWork_4_2
     {
         static void Main(string[] args)
         {
-            int n;
             int m;
+            int n;
             Random rnd = new Random();
             while (true)
             {
-                Console.WriteLine("Введите 2 одиноковых числа");
-                Console.Write("Введите M ");
-                n = int.Parse(Console.ReadLine());
-                Console.Write("Введите N ");
+                Console.WriteLine("Введите два целых одинаковых числа. ");
+                Console.Write("Введите M: ");
                 m = int.Parse(Console.ReadLine());
-                int[,] array1 = new int[m, n];
-                int[,] array2 = new int[m, n];
+                Console.Write("Введите N: ");
+                n = int.Parse(Console.ReadLine());
+                int[,] array1 = new int[m, n];  
+                int[,] array2 = new int[n, m];  
                 if (m == n)
                 {
-                    for (int x = 0; x < m; x++)
+                    for (int x = 0; x < m; x++)     
                     {
-                        for (int y = 0; y < n; y++)
+                        for (int y = 0; y < n; y++)  
                         {
                             array1[x, y] = rnd.Next(1, 20);
-                            Console.Write(array1[x, y]);
+                            Console.Write(array1[x, y] + "\t");
                         }
                         Console.WriteLine();
                     }
-                    Console.WriteLine("Зеркальная матрица");
+                    Console.WriteLine("Отзеркаливаем матрицу: ");
                     for (int x = 0; x < m; x++)
                     {
                         for (int y = 0; y < n; y++)
                         {
-                            array2[x, y] = array1[(n - 1) - x, y];
-                            Console.Write(array2[y, x]);
+                            array2[x, y] = array1[y, x];
+                            Console.Write(array2[x, y] + "; ");
                         }
                         Console.WriteLine();
                     }
-                    Console.WriteLine("Завершено");
+                    Console.WriteLine("Процесс завершён!");
                     Console.ReadKey();
                 }
                 if (m != n)
                 {
-                    Console.WriteLine("Вы ввели неправильные числа!\n");
+                    Console.WriteLine("Неквадратно!\n");
                 }
-
             }
         }
     }
